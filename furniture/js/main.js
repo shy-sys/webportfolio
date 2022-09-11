92a58b17e61a310ec7196da467749d180ddc8f03
shy-sys <73788395+shy-sys@users.noreply.github.com>
2022-09-12 00:34:21 +0900

    init

---
+++ furniture/js/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+{
+$(function(){
+    /*=================================================
+    ハンバーガーメニュー
+    ===================================================*/
+    // ハンバーガーメニューのクリックイベント
+    $('.toggle_btn').on('click', function() {
+      // #headerにopenクラスが存在する場合
+    if ($('#header').hasClass('open')) {
+        // openクラスを削除 メニューが非表示になる
+        $('#header').removeClass('open');
+
+      // #headerにopenクラスが存在しない場合
+    } else {
+        // openクラスを追加、メニューが表示される
+        $('#header').addClass('open');
+    }
+    });
+
+    // メニューが表示されている時に画面をクリックした場合
+    $('#mask').on('click', function() {
+      // openクラスを削除して、メニューを閉じる
+    $('#header').removeClass('open');
+    });
+});
+
+}
